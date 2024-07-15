779a2580dfaf69a04038a542f053907d0d3b3e4a
max410011 <410011max@gmail.com>
2024-07-15 16:50:35 +0000

    update scripts

--- fake_quant/scripts.sh
+++ fake_quant/scripts.sh
@@ -1,3 +1,8 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --model meta-llama/Llama-2-7b-hf \
 --rotate --a_bits 4 --v_bits 4 --k_bits 4 --w_bits 4 --w_clip --w_rtn
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py \
+--model meta-llama/Llama-2-7b-hf \
+--rotate
